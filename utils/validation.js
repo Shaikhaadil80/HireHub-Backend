52c807c8c50e7b902a0d6cab239e6c3dbd389c2a
Shaikhaadil80 <Shaikhaadil80@gmail.com>
2025-11-05 16:34:57 +0530

    validation changes

--- utils/validation.js
+++ utils/validation.js
@@ -39,21 +39,21 @@ const registerValidation = (data) => {
         'any.required': 'Email is required'
       }),
 
-    password: Joi.string()
-      .min(6)
-      .required()
-      .messages({
-        'string.min': 'Password must be at least 6 characters long',
-        'any.required': 'Password is required'
-      }),
+    // password: Joi.string()
+    //   .min(6)
+    //   .required()
+    //   .messages({
+    //     'string.min': 'Password must be at least 6 characters long',
+    //     'any.required': 'Password is required'
+    //   }),
 
-    confirmPassword: Joi.string()
-      .valid(Joi.ref('password'))
-      .required()
-      .messages({
-        'any.only': 'Passwords do not match',
-        'any.required': 'Please confirm your password'
-      }),
+    // confirmPassword: Joi.string()
+    //   .valid(Joi.ref('password'))
+    //   .required()
+    //   .messages({
+    //     'any.only': 'Passwords do not match',
+    //     'any.required': 'Please confirm your password'
+    //   }),
 
     userType: Joi.string()
       .valid('candidate', 'employer', 'admin')
