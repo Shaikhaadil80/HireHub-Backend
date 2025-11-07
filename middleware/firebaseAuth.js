ee5073e8d56f083354a2b8604d077c6b0bd6721b
Shaikhaadil80 <Shaikhaadil80@gmail.com>
2025-11-07 16:49:19 +0530

    check

--- middleware/firebaseAuth.js
+++ middleware/firebaseAuth.js
@@ -1,85 +1,12 @@
-// // const admin = require('firebase-admin');
-// // const User = require('../models/User');
-
-// // // Initialize Firebase Admin (you'll need to set up Firebase Admin SDK)
-// // const serviceAccount = require('../config/firebase-service-account.json');
-
-// // if (!admin.apps.length) {
-// //   admin.initializeApp({
-// //     credential: admin.credential.cert(serviceAccount)
-// //   });
-// // }
-
-// // const verifyFirebaseToken = async (req, res, next) => {
-// //   try {
-// //     let token;
-
-// //     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
-// //       token = req.headers.authorization.split(' ')[1];
-// //     }
-
-// //     if (!token) {
-// //       return res.status(401).json({
-// //         success: false,
-// //         error: 'No Firebase ID token provided'
-// //       });
-// //     }
-
-// //     // Verify Firebase token
-// //     const decodedToken = await admin.auth().verifyIdToken(token);
-// //     req.firebaseUser = decodedToken;
-
-// //     // Find or create user in our database
-// //     let user = await User.findOne({ uid: decodedToken.uid });
-    
-// //     if (!user) {
-// //       // User doesn't exist in our DB yet, but we'll allow the request
-// //       // The frontend should call createUser after Firebase auth
-// //       req.user = null;
-// //     } else {
-// //       req.user = user;
-// //     }
-
-// //     next();
-// //   } catch (error) {
-// //     console.error('Firebase auth error:', error);
-// //     return res.status(401).json({
-// //       success: false,
-// //       error: 'Invalid or expired Firebase token'
-// //     });
-// //   }
-// // };
-
-// // const requireUserInDB = async (req, res, next) => {
-// //   if (!req.user) {
-// //     return res.status(404).json({
-// //       success: false,
-// //       error: 'User profile not found. Please complete registration.'
-// //     });
-// //   }
-// //   next();
-// // };
-
-// // module.exports = {
-// //   verifyFirebaseToken,
-// //   requireUserInDB
-// // };
-
-
-
-
-
 // const admin = require('firebase-admin');
 // const User = require('../models/User');
 
-// // Initialize Firebase Admin using environment variables
+// // Initialize Firebase Admin (you'll need to set up Firebase Admin SDK)
+// const serviceAccount = require('../config/firebase-service-account.json');
+
 // if (!admin.apps.length) {
 //   admin.initializeApp({
-//     credential: admin.credential.cert({
-//       projectId: process.env.FIREBASE_PROJECT_ID,
-//       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-//       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-//     })
+//     credential: admin.credential.cert(serviceAccount)
 //   });
 // }
 
@@ -94,7 +21,7 @@
 //     if (!token) {
 //       return res.status(401).json({
 //         success: false,
-//         error: `No Firebase ID token provided \n ${req.headers.authorization} `
+//         error: 'No Firebase ID token provided'
 //       });
 //     }
 
@@ -102,10 +29,12 @@
 //     const decodedToken = await admin.auth().verifyIdToken(token);
 //     req.firebaseUser = decodedToken;
 
-//     // Find user in our database
+//     // Find or create user in our database
 //     let user = await User.findOne({ uid: decodedToken.uid });
     
 //     if (!user) {
+//       // User doesn't exist in our DB yet, but we'll allow the request
+//       // The frontend should call createUser after Firebase auth
 //       req.user = null;
 //     } else {
 //       req.user = user;
@@ -114,22 +43,6 @@
 //     next();
 //   } catch (error) {
 //     console.error('Firebase auth error:', error);
-    
-//     // Handle specific Firebase errors
-//     if (error.code === 'auth/id-token-expired') {
-//       return res.status(401).json({
-//         success: false,
-//         error: 'Token expired. Please login again.'
-//       });
-//     }
-    
-//     if (error.code === 'auth/id-token-revoked') {
-//       return res.status(401).json({
-//         success: false,
-//         error: 'Token revoked. Please login again.'
-//       });
-//     }
-
 //     return res.status(401).json({
 //       success: false,
 //       error: 'Invalid or expired Firebase token'
@@ -147,27 +60,19 @@
 //   next();
 // };
 
-// // Optional: Get Firebase user details
-// const getFirebaseUser = async (uid) => {
-//   try {
-//     const userRecord = await admin.auth().getUser(uid);
-//     return userRecord;
-//   } catch (error) {
-//     console.error('Error getting Firebase user:', error);
-//     return null;
-//   }
-// };
-
 // module.exports = {
 //   verifyFirebaseToken,
-//   requireUserInDB,
-//   getFirebaseUser
+//   requireUserInDB
 // };
+
+
+
+
 
 const admin = require('firebase-admin');
 const User = require('../models/User');
 
-// Initialize Firebase Admin
+// Initialize Firebase Admin using environment variables
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
@@ -189,40 +94,28 @@ const verifyFirebaseToken = async (req, res, next) => {
     if (!token) {
       return res.status(401).json({
         success: false,
-        error: 'No Firebase ID token provided'
+        error: `No Firebase ID token provided \n ${req.headers.authorization} `
       });
     }
 
     // Verify Firebase token
     const decodedToken = await admin.auth().verifyIdToken(token);
-    
-    // Set firebase user data
-    req.firebaseUser = {
-      uid: decodedToken.uid,
-      email: decodedToken.email,
-      email_verified: decodedToken.email_verified || false
-    };
+    req.firebaseUser = decodedToken;
 
     // Find user in our database
     let user = await User.findOne({ uid: decodedToken.uid });
     
-    if (user) {
-      req.user = {
-        id: user._id,
-        uid: user.uid,
-        email: user.email,
-        userName: user.userName,
-        userType: user.userType,
-        isActive: user.isActive
-      };
-    } else {
+    if (!user) {
       req.user = null;
+    } else {
+      req.user = user;
     }
 
     next();
   } catch (error) {
     console.error('Firebase auth error:', error);
     
+    // Handle specific Firebase errors
     if (error.code === 'auth/id-token-expired') {
       return res.status(401).json({
         success: false,
@@ -230,6 +123,13 @@ const verifyFirebaseToken = async (req, res, next) => {
       });
     }
     
+    if (error.code === 'auth/id-token-revoked') {
+      return res.status(401).json({
+        success: false,
+        error: 'Token revoked. Please login again.'
+      });
+    }
+
     return res.status(401).json({
       success: false,
       error: 'Invalid or expired Firebase token'
@@ -244,39 +144,22 @@ const requireUserInDB = async (req, res, next) => {
       error: 'User profile not found. Please complete registration.'
     });
   }
-  
-  if (!req.user.isActive) {
-    return res.status(401).json({
-      success: false,
-      error: 'User account is deactivated.'
-    });
-  }
-  
   next();
 };
 
-// Enhanced authorization middleware
-const authorize = (...roles) => {
-  return (req, res, next) => {
-    if (!req.user) {
-      return res.status(401).json({
-        success: false,
-        error: 'Authentication required'
-      });
-    }
-
-    if (!roles.includes(req.user.userType)) {
-      return res.status(403).json({
-        success: false,
-        error: `User role ${req.user.userType} is not authorized to access this route`
-      });
-    }
-    next();
-  };
+// Optional: Get Firebase user details
+const getFirebaseUser = async (uid) => {
+  try {
+    const userRecord = await admin.auth().getUser(uid);
+    return userRecord;
+  } catch (error) {
+    console.error('Error getting Firebase user:', error);
+    return null;
+  }
 };
 
 module.exports = {
   verifyFirebaseToken,
   requireUserInDB,
-  authorize
+  getFirebaseUser
 };
