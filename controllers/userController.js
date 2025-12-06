3f6386dde417f109d893b05ba82043f19e86b34d
Shaikhaadil80 <Shaikhaadil80@gmail.com>
2025-12-06 17:53:54 +0530

    changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -331,6 +331,7 @@ const deleteUser = async (req, res) => {
         profileImageUrl: "",
         profileImageThumbUrl: "",
         mobileNo : "0000000000",
+        email : "deleted@email.com",
         deletedAt: Date.now(),
         deletedBy: req.firebaseUser.uid,
         updatedBy: req.firebaseUser.uid
